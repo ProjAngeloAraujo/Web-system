a539780c516cf1734e42323c846e366a6e085193
Angelo <angelo.souza23@etec.sp.gov.br>
2026-04-03 12:38:12 -0300

    Organização e estruturação do projeto

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
