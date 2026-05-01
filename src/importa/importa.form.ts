4b28344b0d6d7226c8841dbf9366a595dd021ecd
Angelo <angelo.souza23@etec.sp.gov.br>
2026-04-30 21:56:02 -0300

    inicinado consumo de API no ts

---
+++ src/importa/importa.form.ts
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const importa = document.querySelector('#botao-importa') as HTMLElement;
+
+    if(!importa) {
+        throw new Error("Botão de importar não encontrado");
+    }
+
+    importa.addEventListener('click', () => {
+
+    })
+});
